8926350058f563c3f2b4ac64e969611203eb3064
Biohazard86 <52355093+Biohazard86@users.noreply.github.com>
2021-01-08 19:13:23 +0100

    Update coma_fija.c
    aniado algunas funciones utiles para el tratamiento de los numeros en coma fija.

--- coma_fija.c
+++ coma_fija.c
@@ -47,8 +47,94 @@ para 2. (...) seria 02
 por lo que el numero final es 
 01110.110 -> 01110110 = 3.14
 
+En nuestro caso la 'ultima posicion (empezando a contar por la izquierda), esta reservada para el signo, las diez anteriores para la parte entera
+el resto se usara para la parte decimal.
+La cifra mas significativa es la de la derecha del vector, y la menos significativa la de la izquierda.
 
 
 */
+// Librerias usadas
+#include <stdlib.h>
+#include <stdio.h>
+
+// CONSTANTES usadas
+#define TAM_VECTOR 1000
+#define NUM_ENTERO 10
+#define NUM_SIGNO 1
+#define NUM_DECIMAL 989
+
+//VARIABLES GLOBALES usadas
+int carry = 0;
 
 
+int funcion_suma (int num1[TAM_VECTOR], int num2[TAM_VECTOR]){
+    //Funcion para sumar dos numeros ordenados como se nos indica en el enunciado
+    // Tenemos la VARIABLE global carry que nos indica si hay acarreo en la operacion realizada. Siempre se debe poner a 0 al acabar.
+    int i, j;
+    int num_final[TAM_VECTOR];
+
+    for(i = 0; i < TAM_VECTOR-1; i++){          // Recordamos que el 'ultimo "bit" o numero es el reservado para el signo
+        num_final[i] = num1[i]+num2[i];
+        if(carry == 1){                         // Comprbamos si hay acarreo en la operacion anterior
+            num_final[i] = num_final[i] + 1;    // Si hay acarreo le sumamos uno al resultado
+            carry = 0;                          // Ponemos a 0 el acarreo
+        }
+        if(num_final[i] > 1){                   // Si el numero que hemos obtenido es mayor a 1, ponemos un 1 y aumentamos el acarreo
+            num_final[i] = 1;                      // Ponemos un 1
+            carry = 1;                          // Ponemos un 1 en el acarreo
+        }
+    }   
+
+}
+
+
+int invertir_array(int *cadena1){
+    int i, array_auxiliar[TAM_VECTOR];
+    //Copiamos de forma inversa 
+    for(i=0; i<TAM_VECTOR; i++){
+        array_auxiliar[TAM_VECTOR-1-i] = cadena1[i];
+    }
+    // Cuando acabemos copiamos el array invertido en cadena de nuevo
+    for(i=0; i<TAM_VECTOR; i++){
+        cadena1[i] = array_auxiliar[i];
+    }
+
+}
+
+
+int main (int argv, char *argc[]){
+    
+    int num_cinco[TAM_VECTOR];
+    int num_dos[2] = {1,2};
+    int i,j;
+
+    int num1[TAM_VECTOR];
+
+    //Guardamos el numero cinco en el vector
+    for(i = 0; i<TAM_VECTOR;i++){
+       
+        if((i == 0) || (i == 1) || (i == 3)){
+            num_cinco[i] = 1; 
+        }
+        else{
+            num_cinco[i] = 0;
+        }
+    }
+
+    //Lo mostramos por pantalla
+    printf("\nNumero 5 >>");
+    printf("\nEl tam es de: %ld\n", sizeof(num_cinco));
+    for(i = 0; i < TAM_VECTOR-1; i++){
+        printf("%d  ", num_cinco[i]);
+    }
+
+
+
+    invertir_array(num_cinco);
+    printf("\nEl tam es de: %ld\n", sizeof(num_cinco));
+    for(i = 0; i < TAM_VECTOR; i++){
+        printf("%d  ", num_cinco[i]);
+    }
+
+    
+}
