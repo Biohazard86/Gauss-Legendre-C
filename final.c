c4433864b7887d2be2e4a689d8ffdc6ffb08a3f6
Biohazard86 <52355093+Biohazard86@users.noreply.github.com>
2021-01-17 12:57:57 +0100

    Update final.c

--- final.c
+++ final.c
@@ -10,6 +10,7 @@
 // CONSTANTES usadas
 #define TAM_VECTOR 22241         // Longitud total del array
 #define NUM_ENTERO 1             // Longitud solo de la parte de entero
+#define BASE 10
 
 //VARIABLES GLOBALES usadas
 int carry = 0;                  // Variable usada para saber si se produce acarreo en la operacione
@@ -118,26 +119,223 @@ int funcion_resta_bien (int cadena1_buena[], int cadena2_buena[], int resultado[
 
 // ==============================================================================================================================================
 // Funcion para realizar multiplicaciones de numeros grandes.
-
+/*
 void karatsuba(int cadena1_buena[], int cadena2_buena[], int resultado[]){
-    int i, j;
-    int cadena1[TAM_VECTOR], cadena2[TAM_VECTOR];
+    int i, j, mitad_redondeado_arriba;
+    int a[TAM_VECTOR], b[TAM_VECTOR];
+    
+    if((TAM_VECTOR%2) >= 1){
+        mitad_redondeado_arriba = ((TAM_VECTOR/2) +1);
+        //printf("\nMitad redondeado %d\n", mitad_redondeado_arriba);
+    }
+    else{
+        mitad_redondeado_arriba = (TAM_VECTOR/2);
+        //printf("\nMitad redondeado %d\n", mitad_redondeado_arriba);
+    }
+    
+    // VARIABLES USADAS POR EL ALGORITMO DE KARATSUBA
+    int c[TAM_VECTOR*2];
+    int a1[mitad_redondeado_arriba], a2[mitad_redondeado_arriba], b1[mitad_redondeado_arriba], b2[mitad_redondeado_arriba];
+    int c1[mitad_redondeado_arriba], c2[mitad_redondeado_arriba], c3[mitad_redondeado_arriba], c4[mitad_redondeado_arriba];
+    int s; // Signo
+    //--------
 
     // Copiamos los vectores en unos auxiliares para no modificarlos
     for(i=0; i< TAM_VECTOR; i++){
-        cadena1[i] = cadena1_buena[i];
-        cadena2[i] = cadena2_buena[i];
+        a[i] = cadena1_buena[i];
+        b[i] = cadena2_buena[i];
     }
 
-    invertir_array(cadena1);        // Colocamos los arrays en el orden normal para realizar las operaciones
-    invertir_array(cadena2);
+    invertir_array(a);        // Colocamos los arrays en el orden normal para realizar las operaciones
+    invertir_array(b);
 
+    // Algoritmo de KARATSUBA
+    if(n == 1){
+        c[0] = a[0] * b[0];
+        c[1] = c[0] / BASE;
+        c[0] = c[0] % BASE;
+    }
+    else
+    {
+        
+    }
+    
 
 
 
 
     invertir_array(resultado);
 }
+
+*/
+
+// Karatsuba >> Es un algortimo para multuplicar numeros grandes de forma eficiente.
+
+unsigned long long pe;
+
+unsigned long long mem,maxmem;
+
+//Funcion para pedir memoria
+int *pidemem(int n)
+{
+	mem+n;
+	return (int *)calloc(n,sizeof(int));
+}
+// Funcion para liberar memoria
+int liberamem(int *p,int k)
+{
+	mem-=k;
+	free(p);
+}
+
+
+void sumal(int *a, int n, int *b, int m, int k)
+{
+
+	int l,carry=0;
+	for(l=0;l<m;++l)
+	{
+		a[l+k]+=b[l]+carry;
+		carry=a[l+k]/BASE;
+		a[l+k]%=BASE;
+	}
+	if(carry>0) a[m+k]+=carry;
+}
+//de el resultado en a , devuelve el acarreo
+int restal(int *a, int n, int *b, int m, int k)
+{
+
+	int l,carry=0;
+	for(l=0;l<m;++l)
+	{
+		a[l+k]-=b[l]+carry;
+		if(a[l+k]<0)
+		{
+			a[l+k]+=BASE;
+			carry=1;
+		}
+		else carry=0;
+		++l;
+	}
+	return carry;
+}
+// Compara
+int compara(int *a, int *b, int n)
+{
+	int i;
+  	for(i=n-1;i>=0;--i)
+  	{
+    	if(a[i]>b[i]) return 1;
+   		if(a[i]<b[i]) return -1;
+   	}
+  	return 0;
+}
+int *copia(int *a, int i, int j, int k)
+{	
+	int l,*b=(int *)calloc(k,sizeof(int));
+	for(l=i;l<=j;++l) b[l-i]=a[l];
+	return b;
+
+}
+int *producto(int *a, int *b, int n)
+{
+
+	int *c=(int *)calloc(2*n,sizeof(int));
+	if(n==1)
+	{
+		c[0]=a[0]*b[0];
+		c[1]=c[0]/BASE;
+		c[0]=c[0]%BASE;
+		++pe;
+
+	}
+	else
+	{
+		int k=(n+1)/2;
+		int *a1,*a2,*b1,*b2,*c1,*c2,*c3,*c4;
+		a1=copia(a,0,k-1,k);
+		a2=copia(a,k,n-1,k);
+		b1=copia(b,0,k-1,k);
+		b2=copia(b,k,n-1,k);
+		// productos
+		c1=producto(a1,b1,k);
+		c2=producto(a1,b2,k);
+		c3=producto(a2,b1,k);
+		c4=producto(a2,b2,n-k);
+		// sumas
+		sumal(c,2*n,c1,2*k,0);
+		sumal(c,2*n,c2,2*k,k);
+		sumal(c,2*n,c3,2*k,k);
+		sumal(c,2*n,c4,2*(n-k),2*k);
+		// liberamos la memoria no necesaria
+		free(a1);free(a2);free(b1);free(b2);
+		free(c1);free(c2);free(c3);free(c4);
+	}
+	return c;
+}
+
+int *karatsuba(int *a,int *b, int n)
+{
+	int *c=(int *)calloc(2*n,sizeof(int));
+	if(n==1)
+	{
+		c[0]=a[0]*b[0];
+		c[1]=c[0]/BASE;
+		c[0]=c[0]%BASE;
+		++pe;
+
+	}
+	else
+	{
+		int k=(n+1)/2,s=1;
+		int *a1,*a2,*b1,*b2,*c1,*c2,*c3;
+		a1=copia(a,0,k-1,k);
+		a2=copia(a,k,n-1,k);
+		b1=copia(b,0,k-1,k);
+		b2=copia(b,k,n-1,k);
+
+		c1=karatsuba(a1,b1,k);
+		c2=karatsuba(a2,b2,n-k);
+
+		sumal(c,2*n,c1,2*k,0);
+		sumal(c,2*n,c2,2*(n-k),2*k);
+		sumal(c,2*n,c1,2*k,k);
+		sumal(c,2*n,c2,2*(n-k),k);
+		free(c1),free(c2);
+
+		if(compara(a1,a2,k)>=0)
+		{
+			restal(a1,k,a2,n-k,0);
+			free(a2);
+		}
+		else
+		{
+			restal(a2,k,a1,k,0);
+			free(a1);
+			a1=a2;
+			s=-s;
+		}
+		if(compara(b1,b2,k)>=0)
+		{
+			restal(b1,k,b2,n-k,0);
+			free(b2);
+		}
+		else
+		{
+			restal(b2,k,b1,k,0);
+			free(b1);
+			b1=b2;
+			s=-s;
+		}
+		c3=karatsuba(a1,b1,k);
+		if(s>0) restal(c,2*n,c3,2*k,k);
+		else sumal(c,2*n,c3,2*k,k);
+
+		free(a1);free(b1);free(c3);
+	}
+	return c;
+}
+
 
 
 // ==============================================================================================================================================
